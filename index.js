ad99342a59c0dccd5b98d04a7d81a05c507117c4
M. Brett Ansite <brett@zabaat.com>
2016-11-03 17:02:52 -0700

    getting mfiles docs

--- index.js
+++ index.js
@@ -3,12 +3,41 @@ let _ = require("lodash")
 let mfiles = require("./lib/rest.js").clientSetup()
 
 
-mfiles("http://192.168.1.4","Imagilux");
-mfiles.genAuthToken("serviceAccount","tunacakes")
+mfiles("http://192.168.1.4");
+mfiles.setupGenAuthToken("serviceAccount","tunacakes","Imagilux",()=>{
+    console.log("I finished! love me!")
+    mfiles.getRootViews((err,data)=>{
+        if(err)
+            return console.log("ERR getting root views",err)
+        console.log("DATA!",data)
+        // mfiles.create("document")
+        mfiles.getObjects(1).then(res=>{
+            console.log("your res",res)
+        })
+    })
+})
 //TODO set vault token via  
 // 
 
 module.exports = {
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
